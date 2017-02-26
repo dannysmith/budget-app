3398a9afff1bba51bb9b0a17d69aa6cc949134f4
Danny Smith <hi@danny.is>
2017-02-26 22:42:19 +0000

    Dont add duplicate transactions if duplicate webhooks are recieved

--- models/transaction.rb
+++ models/transaction.rb
@@ -52,53 +52,58 @@ class Transaction
   end
 
   def self.make(username, data)
-    tx_hash = {
-      user: username,
-      monzo_account_id: data['account_id'],
-      amount: data['amount'],
-      account_balance: data['account_balance'],
-      created: data['created'],
-      currency: data['currency'],
-      notes: data['notes'],
-      description: data['description'],
-      monzo_transaction_id: data['id'],
-      monzo_category: data['category'],
-      is_load: data['is_load']
-    }
+    user = User.find_by(username: username)
 
-    if data['counterparty']
-      tx_hash.merge!({
-        counterparty_name: data['counterparty']['name'],
-        counterparty_prefered_name: data['counterparty']['prefered_name'],
-        counterparty_phone: data['counterparty']['number'],
-        counterparty_user_id: data['counterparty']['user_id']
-      })
-    end
+    unless user.transactions.find_by monzo_transaction_id: data['id']
+      tx_hash = {
+        user: username,
+        monzo_account_id: data['account_id'],
+        amount: data['amount'],
+        account_balance: data['account_balance'],
+        created: data['created'],
+        currency: data['currency'],
+        notes: data['notes'],
+        description: data['description'],
+        monzo_transaction_id: data['id'],
+        monzo_category: data['category'],
+        is_load: data['is_load']
+      }
 
-    if data['merchant']
-      tx_hash.merge!({
-        atm: data['merchant']['atm'],
-        online: data['merchant']['online'],
-        merchant_id: data['merchant']['id'],
-        merchant_logo: data['merchant']['logo'],
-        merchant_emoji: data['merchant']['emoji'],
-        merchant_name: data['merchant']['name']
-      })
-
-      if data['merchant']['address']
+      if data['counterparty']
         tx_hash.merge!({
-          merchant_address: data['merchant']['address']['short_formatted'],
-          merchant_city: data['merchant']['address']['city'],
-          merchant_country: data['merchant']['address']['country'],
-          merchant_lat: data['merchant']['address']['latitude'],
-          merchant_long: data['merchant']['address']['longitude'],
-          merchant_zoom: data['merchant']['address']['zoom_level'],
-          merchant_postcode: data['merchant']['address']['postcode'],
-          merchant_region: data['merchant']['address']['region'],
+          counterparty_name: data['counterparty']['name'],
+          counterparty_prefered_name: data['counterparty']['prefered_name'],
+          counterparty_phone: data['counterparty']['number'],
+          counterparty_user_id: data['counterparty']['user_id']
         })
       end
+
+      if data['merchant']
+        tx_hash.merge!({
+          atm: data['merchant']['atm'],
+          online: data['merchant']['online'],
+          merchant_id: data['merchant']['id'],
+          merchant_logo: data['merchant']['logo'],
+          merchant_emoji: data['merchant']['emoji'],
+          merchant_name: data['merchant']['name']
+        })
+
+        if data['merchant']['address']
+          tx_hash.merge!({
+            merchant_address: data['merchant']['address']['short_formatted'],
+            merchant_city: data['merchant']['address']['city'],
+            merchant_country: data['merchant']['address']['country'],
+            merchant_lat: data['merchant']['address']['latitude'],
+            merchant_long: data['merchant']['address']['longitude'],
+            merchant_zoom: data['merchant']['address']['zoom_level'],
+            merchant_postcode: data['merchant']['address']['postcode'],
+            merchant_region: data['merchant']['address']['region'],
+          })
+        end
+      end
+      user.transactions << self.create(tx_hash)
+    else
+      STDERR.puts "Transaction already exists"
     end
-    user = User.find_by(username: username)
-    user.transactions << self.create(tx_hash)
   end
 end
